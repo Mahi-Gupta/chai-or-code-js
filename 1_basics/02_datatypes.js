9a2f084e03722afc33032ea6ffd51d15231b149b
Mahi-Gupta <145583262+Mahi-Gupta@users.noreply.github.com>
2025-06-25 17:15:57 +0000

    .

---
+++ 1_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";  //Treat all JS code as newer version
+
+// alert("hello")  //we are using nodeJS not browser
+
+console.log("Mahi");
+
+let name = "Mahi"
+let age = 20
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint => for very large values
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+console.log(typeof isLoggedIn)
+console.log(typeof 19);
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
